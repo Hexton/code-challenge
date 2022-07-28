d4deab568ab89dd52ca7354039c76f4d60db44f7
Hexton Ho <hextonho@icloud.com>
2022-07-28 22:51:23 +0800

    Complete 12

--- 12 - Check whether a string contains another string and concatenate/app.js
+++ 12 - Check whether a string contains another string and concatenate/app.js
@@ -1,10 +1,12 @@
-// Write a function that takes two numbers (a and b) as argument
-// Sum a and b
-// Return the result
+// Write a function that takes two strings (a and b) as arguments
+// If a contains b, append b to the beginning of a
+// If not, append it to the end
+// Return the concatenation
 function myFunction(a, b) {
-  return a + b;
+  return a.indexOf(b) !== -1 ? b + a : a + b;
 }
 
-console.log(myFunction(1, 2));
-console.log(myFunction(1, 10));
-console.log(myFunction(99, 1));
+console.log(myFunction('cheese', 'cake')); //'cheesecake'
+console.log(myFunction('lips', 's')); //'slips'
+console.log(myFunction('Java', 'script')); //'Javascript'
+console.log(myFunction(' think, therefore I am', 'I')); //'I think, therefore I am'
