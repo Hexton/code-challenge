aa939426924bcefa4e22531c47a4f99ed816432c
Hexton Ho <hextonho@icloud.com>
2022-07-29 01:56:50 +0800

    Complete 23

--- 22 - Insert character after every n characters (backwards)/app.js
+++ 22 - Insert character after every n characters (backwards)/app.js
@@ -2,7 +2,26 @@
 // Beginning at the end of 'a', insert 'b' after every 3rd character of 'a'
 // Return the resulting string
 function myFunction(a, b) {
-  return;
+  let resultStr = '';
+  let idx = 0;
+  for (i = a.length - 1; i > -1; i--) {
+    idx++;
+    resultStr = a[i] + resultStr;
+    if (idx % 3 === 0) {
+      resultStr = b + resultStr;
+    }
+  }
+  return resultStr;
+}
+
+function myFunction(a, b) {
+  let result = [];
+  let rest = a;
+  while (rest.length) {
+    result.unshift(rest.slice(-3));
+    rest = rest.slice(0, -3);
+  }
+  return result.join(b);
 }
 
 console.log(myFunction('1234567', '.')); //'1.234.567'
--- 23 - Find the correct word by incrementing letters in alphabet/app.js
+++ 23 - Find the correct word by incrementing letters in alphabet/app.js
@@ -1,10 +1,23 @@
-// Write a function that takes two numbers (a and b) as argument
-// Sum a and b
-// Return the result
-function myFunction(a, b) {
-  return a + b;
+// Write a function that takes a string as argument
+// As it is, the string has no meaning
+// Increment each letter to the next letter in the alphabet
+// Return the correct word
+function myFunction(str) {
+  let word = '';
+  for (let i = 0; i < str.length; i++) {
+    word += String.fromCharCode(str[i].charCodeAt(0) + 1);
+  }
+  return word;
 }
 
-console.log(myFunction(1, 2));
-console.log(myFunction(1, 10));
-console.log(myFunction(99, 1));
+function myFunction(str) {
+  const arr = [...str];
+  const correctedArray = arr.map((e) =>
+    String.fromCharCode(e.charCodeAt() + 1)
+  );
+  return correctedArray.join('');
+}
+
+console.log(myFunction('bnchmf')); //'coding'
+console.log(myFunction('bgddrd')); //'cheese'
+console.log(myFunction('sdrshmf')); //'testing'
