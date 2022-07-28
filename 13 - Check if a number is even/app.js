82680449801035d8461b833eac9062e6934f496c
Hexton Ho <hextonho@icloud.com>
2022-07-28 23:05:20 +0800

    Complete 14

--- 13 - Check if a number is even/app.js
+++ 13 - Check if a number is even/app.js
@@ -1,10 +1,11 @@
-// Write a function that takes two numbers (a and b) as argument
-// Sum a and b
-// Return the result
-function myFunction(a, b) {
-  return a + b;
+// Write a function that takes a number as argument
+// If the number is even, return true
+// Otherwise, return false
+function myFunction(a) {
+  return a % 2 === 0;
 }
 
-console.log(myFunction(1, 2));
-console.log(myFunction(1, 10));
-console.log(myFunction(99, 1));
+console.log(myFunction(10)); //true
+console.log(myFunction(-4)); //true
+console.log(myFunction(5)); //false
+console.log(myFunction(-111)); //false
--- 14 - How many times does a character occur/app.js
+++ 14 - How many times does a character occur/app.js
@@ -1,10 +1,18 @@
-// Write a function that takes two numbers (a and b) as argument
-// Sum a and b
-// Return the result
+// Write a function that takes two strings (a and b) as arguments
+// Return the number of times a occurs in b
 function myFunction(a, b) {
-  return a + b;
+  return b.split(a).length - 1;
 }
 
-console.log(myFunction(1, 2));
-console.log(myFunction(1, 10));
-console.log(myFunction(99, 1));
+console.log(
+  myFunction('m', 'how many times does the character occur in this sentence?')
+); //2
+console.log(
+  myFunction('h', 'how many times does the character occur in this sentence?')
+); //4
+console.log(
+  myFunction('?', 'how many times does the character occur in this sentence?')
+); //1
+console.log(
+  myFunction('z', 'how many times does the character occur in this sentence?')
+); //0
