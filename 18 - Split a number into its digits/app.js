f8c418573478672327ba587471a13f47763d7a3a
Hexton Ho <hextonho@icloud.com>
2022-07-29 00:00:15 +0800

    Complete 19

--- 18 - Split a number into its digits/app.js
+++ 18 - Split a number into its digits/app.js
@@ -1,10 +1,17 @@
-// Write a function that takes two numbers (a and b) as argument
-// Sum a and b
-// Return the result
-function myFunction(a, b) {
-  return a + b;
+// Write a function that takes a number (a) as argument
+// Split a into its individual digits and return them in an array
+// Tipp: you might want to change the type of the number for the splitting
+function myFunction(a) {
+  let str = a + '';
+  return str.split('').map((ele) => +ele);
 }
 
-console.log(myFunction(1, 2));
-console.log(myFunction(1, 10));
-console.log(myFunction(99, 1));
+function myFunction(a) {
+  const string = a + '';
+  const strings = string.split('');
+  return strings.map((digit) => Number(digit));
+}
+
+console.log(myFunction(10)); //[1,0]
+console.log(myFunction(931)); //[9,3,1]
+console.log(myFunction(193278)); //[9,3,1]
--- 19 - Clear up the chaos behind these strings/app.js
+++ 19 - Clear up the chaos behind these strings/app.js
@@ -1,10 +1,26 @@
-// Write a function that takes two numbers (a and b) as argument
-// Sum a and b
-// Return the result
+// It seems like something happened to these strings
+// Can you figure out how to clear up the chaos?
+// Write a function that joins these strings together such that they form the following words:
+// 'Javascript', 'Countryside', and 'Downtown'
+// You might want to apply basic JS string methods such as replace(), split(), slice() etc
 function myFunction(a, b) {
-  return a + b;
+  let strA = a.charAt(0).toUpperCase() + a.slice(1);
+  let strB = b.split('').reverse().join('');
+  let combineStr = strA + strB;
+  return combineStr.replace('%', '');
 }
 
-console.log(myFunction(1, 2));
-console.log(myFunction(1, 10));
-console.log(myFunction(99, 1));
+function myFunction(a, b) {
+  const func = (x) => x.replace('%', '');
+  const first = func(a);
+  const second = func(b).split('').reverse().join('');
+  return first.charAt(0).toUpperCase() + first.slice(1) + second;
+}
+
+console.log(myFunction('java', 'tpi%rcs')); //'Javascript'
+console.log(myFunction('c%ountry', 'edis')); //'Countryside'
+console.log(myFunction('down', 'nw%ot')); //'Downtown'
+
+//Reverse b
+//Capital first letter
+//Combine a
